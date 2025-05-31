ed1639e8a4df9e0346e462c5b4f63ae74f82bf23
Ram Kaucha <z5367751@ad.unsw.edu.au>
2025-05-31 16:42:45 +1000

    Project addition: Fata

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -2,9 +2,16 @@ import { useTheme } from "next-themes"
 import SocialButton from "./components/SocialButton";
 import { socials } from "./config/socialLinks";
 import ProjectList from "./project/ProjectList";
+import { ChevronDown } from "lucide-react";
+import { useState } from "react";
 
 export default function Home() {
   const { resolvedTheme } = useTheme();
+  const [numProject, setNumProjects] = useState<number>(4);
+
+  const handleSeeMore = () => {
+    setNumProjects(prev => prev + 4);
+  }
 
   return (
     <div className="sm:px-6">
@@ -31,8 +38,12 @@ export default function Home() {
           ))}
         </div>
       </div>
-      <div className="py-5">
-        <ProjectList max={4} />
+      <div className="py-5 items-center justify-center flex flex-col">
+        <ProjectList max={numProject} />
+        <div className="mt-4 text-sm text-gray-700 dark:text-gray-50 cursor-pointer flex flex-row" onClick={handleSeeMore}>
+          <p>See more</p>
+          <ChevronDown className="w-5 h-5"/>
+        </div>
       </div>
       <div className="py-5">
         <p className="text-[2em] font-bold">
